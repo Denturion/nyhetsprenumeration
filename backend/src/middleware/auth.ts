73aa10a90dde32b8722f25a4869bdd004e5a66cb
David Funck <xdavidfunckx@gmail.com>
2025-05-30 10:13:58 +0200

    Added authentication

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const authenticateJWT = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const authHeader = req.headers.authorization;
+
+	if (!authHeader || !authHeader.startsWith('Bearer')) {
+		return res.status(401).json({ message: 'No token provided' });
+	}
+
+	const token = authHeader.split('')[1];
+
+	try {
+		const secret = process.env.JWT_SECRET || 'secret';
+		const decoded = jwt.verify(token, secret);
+		req.user = decoded;
+		next();
+	} catch (error) {
+		return res.status(401).json({ message: 'Invalid or expired token' });
+	}
+};
