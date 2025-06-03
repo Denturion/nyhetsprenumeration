3bc18793a376e2dc4cc55c90623997fc11ef113c
Viktor Linné <viktor.linne@medieinstitutet.se>
2025-06-03 13:14:19 +0200

    added logs for finding error in when webhook broke

--- backend/src/routes/webhookRoutes.ts
+++ backend/src/routes/webhookRoutes.ts
@@ -4,6 +4,18 @@ import { handleStripeWebhook } from "../controllers/webhookController";
 
 const router = express.Router();
 
-router.post("/",bodyParser.raw({ type: "application/json" }), handleStripeWebhook);
+router.post(
+  "/",
+  (req, res, next) => {
+    console.log("🔵 Steg 1: webhook route anropad");
+    next();
+  },
+  bodyParser.raw({ type: "application/json" }), // 👈 MÅSTE vara raw!
+  (req, res, next) => {
+    console.log("🟢 Steg 2: raw body-parser klar");
+    next();
+  },
+  handleStripeWebhook
+);
 
 export default router;
