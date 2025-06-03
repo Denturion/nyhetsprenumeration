73a10e5ad4e7c1de1e3859cead4000553864c456
Viktor Linné <viktor.linne@medieinstitutet.se>
2025-06-03 08:31:44 +0200

    added stripe and webhook baseroutes to index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,42 +1,47 @@
-import express, { Application} from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import articleRouter from './routes/articles';
-import { db } from './config/db';
+import express, { Application } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import articleRouter from "./routes/articles";
+import stripeRoutes from "./routes/stripeRoutes";
+import stripeWebhook from "./routes/webhookRoutes";
+import { db } from "./config/db";
+
+console.log("✅ Servern försöker starta...");
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use("/webhook", stripeWebhook);
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('Backend is running!');
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
 });
 
+const createRoutes = (app: Application): void => {
+  app.use("/articles", articleRouter);
+};
 
-const createRoutes = (app:Application): void => {
-	app.use('/articles', articleRouter );
-	
-}
+app.use("/stripe", stripeRoutes);
 
 createRoutes(app);
 
-app.get('/db-check', async (req, res) => {
-	try {
-		const [rows] = await db.query('SELECT 1');
-		res.json({ success: true, message: 'DB connection OK', rows });
-		console.log('DB connection successful:', rows);
-	} catch (error) {
-		res
-			.status(500)
-			.json({ success: false, message: 'DB connection failed' + error });
-		console.log('DB connection failed:', error);
-	}
+app.get("/db-check", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT 1");
+    res.json({ success: true, message: "DB connection OK", rows });
+    console.log("DB connection successful:", rows);
+  } catch (err: any) {
+    res
+      .status(500)
+      .json({ success: false, message: "DB connection failed" + err.message });
+    console.log("DB connection failed:", err.message);
+  }
 });
 
-
 app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
